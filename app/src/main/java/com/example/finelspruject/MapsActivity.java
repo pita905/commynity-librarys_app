a3b6edb6ac6b778294f364527cdd6279d332483d
itama <itamarhi90@gmai.com>
2025-03-08 20:05:51 +0200

    f23

--- app/src/main/java/com/example/finelspruject/MapsActivity.java
+++ app/src/main/java/com/example/finelspruject/MapsActivity.java
@@ -78,6 +78,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+
         btnMyLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
